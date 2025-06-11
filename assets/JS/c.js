c39c69bd32eab22541c23c8319839e663ebbc0a7
Aymen Oumaalla <aymenoml2002@gmail.com>
2025-06-11 12:43:01 +0100

    Update c.js

--- assets/JS/c.js
+++ assets/JS/c.js
@@ -1,17 +1,10 @@
-// Get all the increment buttons and quantity input fields
-const incrementButtons = document.querySelectorAll('.increment-button');
-const quantityInputs = document.querySelectorAll('.quantity-input');
+let incrementButtons = document.querySelectorAll('.increment-button');
+let quantityInputs = document.querySelectorAll('.quantity-input');
 
-// Add click event listener to each increment button
 incrementButtons.forEach((button, index) => {
   button.addEventListener('click', () => {
-    // Get the current quantity value
     let currentValue = parseInt(quantityInputs[index].value);
-    
-    // Increment the quantity value by 1
     currentValue += 1;
-
-    // Update the quantity input field with the new value
     quantityInputs[index].value = currentValue;
   });
 });
