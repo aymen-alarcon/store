d6e8cefa92536e2e3253811b7bb82dbe8018ace8
Aymen Oumaalla <aymenoml2002@gmail.com>
2025-06-11 12:49:47 +0100

    Delete assets/JS/script2.js

--- assets/JS/script2.js
+++
@@ -1,9 +0,0 @@
-const plusOneButton = document.getElementById("plusOneButton*");
-const countElement = document.getElementById("count");
-
-let count = 0;
-
-plusOneButton.addEventListener("click", () => {
-  count++;
-  countElement.textContent = count;
-});
